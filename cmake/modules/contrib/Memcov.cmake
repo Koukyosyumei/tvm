8cd3ca90c9dd9e4aaf989d35c48b072db1c0927a
Koukyosyumei <koukyosyumei@hotmail.com>
2024-09-01 09:48:14 -0400

    instrument

---
+++ cmake/modules/contrib/Memcov.cmake
@@ -0,0 +1,18 @@
+if(USE_COV)
+    include(FetchContent)
+    FetchContent_Declare(
+      memcov
+      GIT_REPOSITORY "https://github.com/ganler/memcov.git"
+      GIT_TAG        "origin/master"
+    )
+
+    FetchContent_GetProperties(memcov)
+
+    if(NOT memcov_POPULATED)
+      FetchContent_Populate(memcov)
+      add_subdirectory(${memcov_SOURCE_DIR} ${memcov_BINARY_DIR})
+    endif()
+
+    list(APPEND TVM_LINKER_LIBS memcov)
+    list(APPEND TVM_RUNTIME_LINKER_LIBS memcov)
+endif()
